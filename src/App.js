e1a439164f8f681acc647a8985aee4eeb02d4069
iamBilal04 <110116638+iamBilal04@users.noreply.github.com>
2023-03-30 00:31:54 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Header from './components/Header';
+import { TodoWrapper } from './components/TodoWrapper';
+
+
+function App() {
+  return (
+      
+      <div className="App">
+        <Header />
+        <TodoWrapper  />
+      </div>
+
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div className="header">
+        Todo List
+    </div>
+  )
+}
+
+export default Header
